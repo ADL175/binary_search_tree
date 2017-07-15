cf0d6f48fdb23145ef8cb464ad04ca32da3e48e3
ADL175 <armydavidlim@gmail.com>
2017-07-14 23:02:42 -0700

    updated readme, func and test

--- src/test_insertion.py
+++ src/test_insertion.py
@@ -6,15 +6,17 @@ import pytest
 
 def test_bubble():
     """test insertion sort function on list of ints."""
-    a = [123,55,2,7,22,-10,1]
+    a = [123, 55, 2, 7, 22, -10, 1]
     insertion_sort(a)
     assert a == [-10, 1, 2, 7, 22, 55, 123]
+
 
 def test_bubble_empty_input():
     """test insertion sort function on empty list."""
     a = []
     insertion_sort(a)
     assert a == []
+
 
 def test_bubble_letters_input():
     """test insertion sort function on list of strings."""
